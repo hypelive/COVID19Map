5af8fcb27a46824178dad61992e40391c32c32c2
kirillkulemin <50181005+kirillkulemin@users.noreply.github.com>
2020-05-22 14:36:42 +0500

    Add files via upload

---
+++ COVID19Map/DataParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COVID19Map
+{
+    static class DataParser
+    {
+        public static void ParseСoordinates(CountryData country)
+        {}
+        
+        public static void ParseStatistics()
+        {}
+    }
+}
